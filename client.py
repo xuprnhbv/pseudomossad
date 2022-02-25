07081f6084a14225515f3ee4a1fce2afd769f435
ido moshe <www.ido.co.il@gmail.com>
2022-02-25 08:57:48 +0200

    Indentation

--- client.py
+++ client.py
@@ -22,8 +22,10 @@ def query_server(token, password, server=SERVER_ADDRESS):
     :param server: server address and port (address, port)
     :return: a dict of the response. returns empty dict on fail.
     """
-    res = requests.post(SERVER_ADDRESS, data={'token': token, 'password': password})
+    res = requests.post(SERVER_ADDRESS, data={
+                        'token': token, 'password': password})
     return json.loads(res.content.decode('utf-8'))
+
 
 def get_answer(token, password):
     """
@@ -35,7 +37,8 @@ def get_answer(token, password):
     """
     qu = query_server(token, password)
     return qu['isValid'], qu[PICTURE_FIELD]
-        
+
+
 def popup_image(lock_response):
     """
     Creates popup for success.
@@ -46,7 +49,8 @@ def popup_image(lock_response):
     popup.title('Successfull Authentication!')
     popup.resizable(False, False)
     a = Label(popup, text='Server responded with:').grid()
-    img = ImageTk.PhotoImage(Image.open(BytesIO(base64.b64decode(lock_response))).resize(PICTURE_DIMENSIONS))
+    img = ImageTk.PhotoImage(Image.open(
+        BytesIO(base64.b64decode(lock_response))).resize(PICTURE_DIMENSIONS))
     imagepanel = Label(popup, image=img)
     imagepanel.grid(row=1)
     popup.mainloop()
@@ -64,17 +68,20 @@ def click(window):
     text = ''
     fg = ''
     if not token or not password:
-        main_text.configure(text='token and password must be filled.', fg='black')
+        main_text.configure(
+            text='token and password must be filled.', fg='black')
     else:
         try:
             is_valid, response = get_answer(token, password)
             if is_valid:
-                main_text.configure(text='Successfull Authentication!', fg='green')
+                main_text.configure(
+                    text='Successfull Authentication!', fg='green')
                 popup_image(response)
             else:
                 main_text.configure(text='Invalid Authentication!', fg='red')
         except (ConnectionError, requests.exceptions.ConnectionError):
             main_text.configure(text=f'Bad connection :(', fg='red')
+
 
 def setup_ui():
     """
@@ -84,13 +91,18 @@ def setup_ui():
     window.title('SEC')
     window.resizable(False, False)
     window.configure(padx=10, pady=5)
-    a = Label(window, text='Enter your supersecret credentials', name='main_text').grid(columnspan=4)
+    a = Label(window, text='Enter your supersecret credentials',
+              name='main_text').grid(columnspan=4)
     b = Label(window, text='Token:').grid(row=1)
-    token_entry = Entry(window, name='token').grid(row=1, column=1, columnspan=3)
+    token_entry = Entry(window, name='token').grid(
+        row=1, column=1, columnspan=3)
     c = Label(window, text='Password:').grid(row=2)
-    pass_entry = Entry(window, name='password').grid(row=2, column=1, columnspan=3)
-    submit = Button(window, text='Submit', command=lambda: click(window)).grid(row=3, columnspan=2)
+    pass_entry = Entry(window, name='password').grid(
+        row=2, column=1, columnspan=3)
+    submit = Button(window, text='Submit', command=lambda: click(
+        window)).grid(row=3, columnspan=2)
     window.mainloop()
+
 
 if __name__ == '__main__':
     setup_ui()
