f5387a825f09c13acf1711f3cfe769f29d537885
Ido <Ido@DESKTOP-M5BOPQ7>
2022-04-15 23:13:31 +0300

    Removed unnecessaries and added server address textbox

--- client.py
+++ client.py
@@ -2,18 +2,20 @@ from urllib import response
 import requests
 import json
 import base64
+import re
 from tkinter import *
 from tkinter import ttk
 from PIL import Image, ImageTk
 from io import BytesIO
 
 
-SERVER_ADDRESS = 'http://127.0.0.1'
+HTTP_PRE = 'http://{}'
+IP_REGEX = r"^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)(\.(?!$)|$)){4}$"
 PICTURE_FIELD = 'lockResponse'
 PICTURE_DIMENSIONS = (512, 512)
 
 
-def query_server(token, password, server=SERVER_ADDRESS):
+def query_server(token, password, server):
     """
     Sends a request to the server, and returns the parsed response.
 
@@ -22,20 +24,20 @@ def query_server(token, password, server=SERVER_ADDRESS):
     :param server: server address and port (address, port)
     :return: a dict of the response. returns empty dict on fail.
     """
-    res = requests.post(SERVER_ADDRESS, data={
+    res = requests.post(HTTP_PRE.format(server), data={
                         'token': token, 'password': password})
     return json.loads(res.content.decode('utf-8'))
 
 
-def get_answer(token, password):
+def get_answer(token, password, server):
     """
-    Nicer overhaul of query_server.
+    Nicer wrap of query_server.
 
     :param token: the token to send to the server
     :param password: the password to send to the server
     :return: tuple of (is_valid, lock_response)
     """
-    qu = query_server(token, password)
+    qu = query_server(token, password, server)
     return qu['isValid'], qu[PICTURE_FIELD]
 
 
@@ -64,15 +66,16 @@ def click(window):
     """
     token = window.nametowidget('token').get()
     password = window.nametowidget('password').get()
+    address = window.nametowidget('server').get()
     main_text = window.nametowidget('main_text')
-    text = ''
-    fg = ''
-    if not token or not password:
+    if not re.match(IP_REGEX, address):
+        main_text.configure(text="invalid ipv4 address", fg='black')
+    elif not token or not password:
         main_text.configure(
             text='token and password must be filled.', fg='black')
     else:
         try:
-            is_valid, response = get_answer(token, password)
+            is_valid, response = get_answer(token, password, address)
             if is_valid:
                 main_text.configure(
                     text='Successfull Authentication!', fg='green')
@@ -81,6 +84,8 @@ def click(window):
                 main_text.configure(text='Invalid Authentication!', fg='red')
         except (ConnectionError, requests.exceptions.ConnectionError):
             main_text.configure(text=f'Bad connection :(', fg='red')
+        except json.decoder.JSONDecodeError:
+            main_text.configure(text="Invalid response :(", fg='red')
 
 
 def setup_ui():
@@ -91,16 +96,18 @@ def setup_ui():
     window.title('SEC')
     window.resizable(False, False)
     window.configure(padx=10, pady=5)
-    a = Label(window, text='Enter your supersecret credentials',
+    Label(window, text='Enter your supersecret credentials',
               name='main_text').grid(columnspan=4)
-    b = Label(window, text='Token:').grid(row=1)
-    token_entry = Entry(window, name='token').grid(
-        row=1, column=1, columnspan=3)
-    c = Label(window, text='Password:').grid(row=2)
-    pass_entry = Entry(window, name='password').grid(
+    Label(window, text='Server:').grid(row=1)
+    Entry(window, name='server').grid(row=1, column=1, columnspan=3)
+    Label(window, text='Token:').grid(row=2)
+    Entry(window, name='token').grid(
         row=2, column=1, columnspan=3)
-    submit = Button(window, text='Submit', command=lambda: click(
-        window)).grid(row=3, columnspan=2)
+    Label(window, text='Password:').grid(row=3)
+    Entry(window, name='password').grid(
+        row=3, column=1, columnspan=3)
+    Button(window, text='Submit', command=lambda: click(
+        window)).grid(row=4, columnspan=2)
     window.mainloop()
 
 
